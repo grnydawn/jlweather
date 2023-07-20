4617a63237509216e7afab6f81b2ac9dfdb9d79e
Youngsung Kim <youngsung.kim.act2@gmail.com>
2023-07-20 18:10:43 -0400

    revised forthip versions

---
+++ run/crusher/runtest_forthip.sh
@@ -0,0 +1,14 @@
+srun -n 8 -- make jai_forthip1
+sleep 1
+srun -n 8 -- make jai_forthip1
+sleep 1
+
+srun -n 8 -- make jai_forthip2
+sleep 1
+srun -n 8 -- make jai_forthip2
+sleep 1
+
+srun -n 8 -- make jai_forthip3
+sleep 1
+srun -n 8 -- make jai_forthip3
+sleep 1
